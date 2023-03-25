1fb4edad52b998cc5b5a78e8a2fed9baca1b5131
handa <akdnpdl0127@gmail.com>
2023-03-26 02:29:13 +0900

    [aop - 포인트 컷 완료]

---
+++ src/test/java/hello/aop/pointcut/ThisTargetTest.java
@@ -0,0 +1,56 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(ThisTargetTest.ThisTargetAspect.class)
+@SpringBootTest(properties = "spring.aop.proxy-target-class=false")
+public class ThisTargetTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void  successTest(){
+        log.info("memberService proxy = {}", memberService.getClass());
+        memberService.hello("hello A");
+    }
+
+    @Slf4j
+    @Aspect
+    static class ThisTargetAspect {
+
+        @Around("this(hello.aop.member.MemberService)")
+        public Object doThisInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("this - interface = {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(hello.aop.member.MemberService)")
+        public Object doTargetInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("target - interface = {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("this(hello.aop.member.MemberServiceImpl)")
+        public Object doThisImplInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("this Impl - interface = {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(hello.aop.member.MemberServiceImpl)")
+        public Object doTargetImplInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("target Impl - interface = {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+
+}
